928d29b73047d1d3bdbcbcc8a19c19b821ef7f80
JinChaeKim <rlawlsco88@gmail.com>
2025-02-20 16:04:09 +0900

    Design : Navi UI 구성

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className="flex justify-between bg-stone-100 s">
+    <div className="flex justify-between bg-stone-100 shadow-lg">
       <Link to={"/"}>
         <div className="px-10 py-5">Home</div>
       </Link>
