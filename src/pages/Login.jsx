c699689862a139af24f823807d21947006e57700
JinChaeKim <rlawlsco88@gmail.com>
2025-02-20 17:49:57 +0900

    Docs : Input 공통 컴포넌트 생성 및 적용

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,20 @@
 import { Link } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 const Login = () => {
+  const handleLogin = () => {};
   return (
-    <div className="flex mt-10 justify-center items-center ">
+    <form className="flex mt-10 justify-center items-center ">
       <div className="p-5 m-7 h-[450px] w-[400px] bg-gray-50 rounded-lg shadow-xl">
         <p className="p-5 font-medium text-[30px]">로그인</p>
-        <div className="p-5 h-[250px] w-[350px] bg-gray-200 rounded-lg ">
-          <input
-            placeholder="아이디"
-            className="p-3 m-3 w-[270px] rounded-md"
-          />
-          <input
-            placeholder="비밀번호"
-            className="p-3 m-3 w-[270px] rounded-md"
-          />
-          <br />
-          <button className="p-3 m-3 w-[270px] text-white bg-violet-400 rounded-md">
+        <AuthForm mode="login" onSubmit={handleLogin} className="h-[250px]">
+          <button
+            type="submit"
+            className="p-3 m-3 w-[270px] text-white bg-violet-400 rounded-md"
+          >
             로그인
           </button>
-        </div>
+        </AuthForm>
         <div className="flex pt-5">
           <p>계정이 없으신가요?</p>
           <Link to={"/signup"}>
@@ -28,7 +24,7 @@ const Login = () => {
           </Link>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
