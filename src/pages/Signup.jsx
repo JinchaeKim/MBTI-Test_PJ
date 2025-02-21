f66e8a3387f69e42c7eff80a63713365a23dddfb
JinChaeKim <rlawlsco88@gmail.com>
2025-02-22 01:02:55 +0900

    Move : Form으로 이동

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,50 +1,12 @@
 import { Link } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-import api from "../api/auth";
-import axios from "axios";
 
 const Signup = () => {
-  const { id, password, nickname, setNickname } = useContext(AuthContext);
-
-  const handleSignup = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(api, { id, password, nickname });
-      const data = response.data;
-      if (data.success) {
-        navigator("/login");
-      } else {
-        alert("회원가입에 실패했습니다!");
-      }
-    } catch (error) {
-      console.error("Signup error", error);
-      alert("회원가입에 실패했습니다!");
-    }
-  };
   return (
     <div className="flex mt-10 justify-center items-center">
       <div className="p-5 m-7 h-[550px] w-[400px] bg-gray-50 rounded-lg shadow-xl">
         <p className="p-5 font-medium text-[30px]">회원가입</p>
-        <AuthForm onSubmit={handleSignup} className="h-[330px]">
-          <input
-            type="text"
-            value={nickname}
-            onChange={(e) => {
-              setNickname(e.target.value);
-            }}
-            placeholder="닉네임"
-            className="p-3 m-3 w-[270px] rounded-md"
-          />
-          <br />
-          <button
-            type="submit"
-            className="p-3 m-3 w-[270px] text-white bg-violet-400 rounded-md hover:bg-violet-300"
-          >
-            회원가입
-          </button>
-        </AuthForm>
+        <AuthForm mode="signup" className="h-[330px]" />
         <div className="flex pt-5">
           <p>이미 계정이 있으신가요?</p>
           <Link to={"/login"}>
