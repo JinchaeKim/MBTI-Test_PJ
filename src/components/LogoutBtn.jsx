7656e15bc477f18791256596eed1a8b6e72670b9
JinChaeKim <rlawlsco88@gmail.com>
2025-02-22 01:05:54 +0900

    Feat : 로그아웃 버튼 수정 중 - 새로고침 없이 반영

--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const LogoutBtn = () => {
-  return <div>LogoutBtn</div>;
+  const { setIsAuthenticated } = useState(AuthContext);
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <div className="flex justify-center items-center ml-[20px] mr-[60px]">
+      <button
+        onClick={() => logout()}
+        className="flex justify-center items-center text-center h-[40px] w-[90px] px-[20px] rounded-lg text-white font-bold bg-violet-400 hover:bg-violet-300"
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default LogoutBtn;
