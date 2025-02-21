606b05411eef897c3805282742f606e69eba4281
JinChaeKim <rlawlsco88@gmail.com>
2025-02-22 01:01:07 +0900

    Docs : auth 가져오기 수정 - create가 baseURL 포함

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,25 +1,31 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://www.nbcamp-react-auth.link/" });
+const authApi = axios.create({
+  baseURL: "https://www.nbcamp-react-auth.link",
+});
 
+// 회원가입
 export const register = async (userData) => {
-  const response = await axios.post(`${api}/register`, userData);
+  const response = await authApi.post("/register", userData);
   return response.data;
 };
 
+// 로그인
 export const login = async (userData) => {
-  const response = await axios.post(`${api}/login`, userData);
+  const response = await authApi.post("/login", userData);
   return response.data;
 };
 
+// 회원 정보 확인
 export const getUserProfile = async (token) => {
-  const response = await axios.get(`${api}/user`, token);
+  const response = await authApi.get("/user", token);
   return response.data;
 };
 
+// 프로필 변경
 export const updateProfile = async (formData) => {
-  const response = await axios.get(`${api}/profile`, formData);
+  const response = await authApi.patch("/profile", formData);
   return response.data;
 };
 
-export default api;
+export default authApi;
