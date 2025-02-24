19bc2f79ead23f2918b390cd270c9d4c00a216d7
JinChaeKim <rlawlsco88@gmail.com>
2025-02-25 00:47:53 +0900

    Docs : sessionStorage 변경

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const token = localStorage.getItem("accessToken");
+  const token = sessionStorage.getItem("accessToken");
   const [isAuthenticated, setIsAuthenticated] = useState(!!token);
   const [userInfo, setUserInfo] = useState({
     id: "",
