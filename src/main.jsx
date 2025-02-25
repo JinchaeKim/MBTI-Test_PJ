5a05547a998526c449c8f086c497742142f6d336
JinChaeKim <rlawlsco88@gmail.com>
2025-02-25 19:48:20 +0900

    init : tanstanck 설치 및 provider 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new queryClient();
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
   // </StrictMode>,
 );
