4633e5c396e54fbcd11057e17b9f067777c88c65
JinChaeKim <rlawlsco88@gmail.com>
2025-02-25 12:54:18 +0900

    Updatd : json-server 글리치 주소로 변경

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const jsonApi = "http://localhost:5000/testResults";
+const jsonApi = "https://ambiguous-emphasized-chance.glitch.me;
 
 export const getTestResults = async () => {
   const response = await axios.get(jsonApi);
